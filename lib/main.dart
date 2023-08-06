8b7433f4426e90b3e48a15689583e05bff89d6ed
mohamad mahvash <mohamadmahvash1@gmail.com>
2023-08-06 17:57:55 +0330

    add fluttergen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tech_application/gen/assets.gen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,10 +12,29 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      localizationsDelegates: [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: [
+    Locale('fa'), // persian
+  ],
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image(image: Assets.images.splashLogo.provider()),
       ),
     );
   }
